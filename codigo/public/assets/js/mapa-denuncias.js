347b35d5a4d8d02595829a47d957f2fe6929eed5
bernardo <bernardoramosf22@gmail.com>
2026-05-11 19:53:23 -0300

    fix: modifica o get na rota de heatmap

--- codigo/public/assets/js/mapa-denuncias.js
+++ codigo/public/assets/js/mapa-denuncias.js
@@ -1,20 +1,6 @@
-async function carregarHeatmap() {       
-  try {                                  
-    const response = await fetch(       
-      "http://localhost:3000/heatmap"
-    );
-    
-    const data = await response.json(); 
+(async () => {
+  const response = await fetch('http://localhost:3000/heatmap');
 
-    const locais = data.complaints.map(coord => ({  
-      lat: parseFloat(coord[0]),         
-      lng: parseFloat(coord[1])          
-    }));
-
-    console.log(locais);                 
-    return locais;                       
-
-  } catch (error) {                      
-    console.error("Erro:", error);       
-  }
-}
+  const heatmapData = await response.json();
+  console.log(heatmapData);
+})();
