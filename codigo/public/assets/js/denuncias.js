f167f708083e9458796efe98fb29fded87a81f6e
Gabriel Oliveira <gabrieloliveiracostaaz@gmail.com>
2026-05-10 23:55:30 -0300

    style: finaliza estilização do modal de filtro de denúncias

--- codigo/public/assets/js/denuncias.js
+++ codigo/public/assets/js/denuncias.js
@@ -1,3 +1,24 @@
-function abrirFiltro() {
-    alert("IHUUUUUUUUUU!");
-};
+const overlay = document.getElementById('overlay');
+const btnFiltrar = document.getElementById('btn-filtrar');
+const btnFechar = document.getElementById('btn-fechar');
+const btnCancelar = document.getElementById('btn-cancelar');
+const modal = document.getElementById('modal');
+
+btnFiltrar.addEventListener('click', function() {
+    overlay.classList.add('aberto');
+});
+
+btnFechar.addEventListener('click', function() {
+    overlay.classList.remove('aberto');
+});
+
+btnCancelar.addEventListener('click', function() {
+    overlay.classList.remove('aberto');
+});
+
+
+overlay.addEventListener('click', function(evento) {
+    if (evento.target === overlay) {
+    overlay.classList.remove('aberto');
+    }
+});
