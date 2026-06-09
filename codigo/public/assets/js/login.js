ef71a5454d45bbeeaabab7c5316daaf21efeddde
Gabriel Oliveira <gabrieloliveiracostaaz@gmail.com>
2026-06-09 15:11:55 -0300

    feat: adiciona validação de campos vazios no login

--- codigo/public/assets/js/login.js
+++ codigo/public/assets/js/login.js
@@ -1,18 +1,27 @@
-    const botaoEntrar = document.getElementById("botao-entrar")
-    const botaoOlho = document.getElementById("botao-olho")
-    const inputSenha = document.getElementById("senha")
+const botaoEntrar = document.getElementById("botao-entrar")
+const botaoOlho = document.getElementById("botao-olho")
+const inputSenha = document.getElementById("senha")
 
 
-    botaoOlho.addEventListener("click", function(){
-        if(inputSenha.type === "password"){
-            inputSenha.type = "text"
-        } else {
-            inputSenha.type = "password"
-        }
-    })
+botaoOlho.addEventListener("click", function(){
+if(inputSenha.type === "password"){
+    inputSenha.type = "text"
+} else {
+    inputSenha.type = "password"
+    }
+})
 
     botaoEntrar.addEventListener('click', async function() {
     const nome = document.getElementById('nome').value.trim()
     const usuario = document.getElementById('usuario').value.trim()
     const senha = document.getElementById('senha').value.trim()
-})
+
+        if (!nome || !usuario || !senha) {
+        document.getElementById('erro-geral').textContent = 'Preencha todos os campos.'
+        document.getElementById('erro-geral').style.display = 'block'
+        return
+    }
+
+    document.getElementById('erro-geral').style.display = 'none'
+    })
+
