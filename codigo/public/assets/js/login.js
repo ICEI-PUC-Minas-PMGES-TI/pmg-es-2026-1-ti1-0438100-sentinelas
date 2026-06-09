3ff8189d5230437e29ec818d536429efa0b88fa6
Gabriel Oliveira <gabrieloliveiracostaaz@gmail.com>
2026-06-09 15:06:04 -0300

    feat: adiciona captura dos campos no clique do botão entrar

--- codigo/public/assets/js/login.js
+++ codigo/public/assets/js/login.js
@@ -1,12 +1,18 @@
-const botaoEntrar = document.getElementById("botao-entrar")
-const botaoOlho = document.getElementById("botao-olho")
-const inputSenha = document.getElementById("senha")
+    const botaoEntrar = document.getElementById("botao-entrar")
+    const botaoOlho = document.getElementById("botao-olho")
+    const inputSenha = document.getElementById("senha")
 
 
-botaoOlho.addEventListener("click", function(){
-    if(inputSenha.type === "password"){
-        inputSenha.type = "text"
-    } else {
-        inputSenha.type = "password"
-    }
+    botaoOlho.addEventListener("click", function(){
+        if(inputSenha.type === "password"){
+            inputSenha.type = "text"
+        } else {
+            inputSenha.type = "password"
+        }
+    })
+
+    botaoEntrar.addEventListener('click', async function() {
+    const nome = document.getElementById('nome').value.trim()
+    const usuario = document.getElementById('usuario').value.trim()
+    const senha = document.getElementById('senha').value.trim()
 })
