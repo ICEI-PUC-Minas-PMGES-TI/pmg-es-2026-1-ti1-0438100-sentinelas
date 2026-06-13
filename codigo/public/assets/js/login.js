de02d9221c829158d92110655e4d53185e7ebc91
Gabriel Oliveira <gabrieloliveiracostaaz@gmail.com>
2026-06-13 17:36:56 -0300

    feat: implementa comparação de credenciais e redirecionamento após login

--- codigo/public/assets/js/login.js
+++ codigo/public/assets/js/login.js
@@ -3,20 +3,20 @@ const botaoOlho = document.getElementById("botao-olho")
 const inputSenha = document.getElementById("senha")
 
 
-botaoOlho.addEventListener("click", function(){
-if(inputSenha.type === "password"){
-    inputSenha.type = "text"
-} else {
-    inputSenha.type = "password"
+botaoOlho.addEventListener("click", function () {
+    if (inputSenha.type === "password") {
+        inputSenha.type = "text"
+    } else {
+        inputSenha.type = "password"
     }
 })
 
-    botaoEntrar.addEventListener('click', async function() {
+botaoEntrar.addEventListener('click', async function () {
     const nome = document.getElementById('nome').value.trim()
     const usuario = document.getElementById('usuario').value.trim()
     const senha = document.getElementById('senha').value.trim()
 
-        if (!nome || !usuario || !senha) {
+    if (!nome || !usuario || !senha) {
         document.getElementById('erro-geral').textContent = 'Preencha todos os campos.'
         document.getElementById('erro-geral').style.display = 'block'
         return
@@ -25,15 +25,34 @@ if(inputSenha.type === "password"){
     document.getElementById('erro-geral').style.display = 'none'
 
     try {
-    const respostaUsuarios = await fetch('http://localhost:3000/usuarios')
-    const usuarios = await respostaUsuarios.json()
+        const respostaUsuarios = await fetch('http://localhost:3000/usuarios')
+        const usuarios = await respostaUsuarios.json()
 
-    const respostaAgentes = await fetch('http://localhost:3000/agents')
-    const agentes = await respostaAgentes.json()
+        const respostaAgentes = await fetch('http://localhost:3000/agents')
+        const agentes = await respostaAgentes.json()
 
-} catch (erro) {
-    document.getElementById('erro-geral').textContent = 'Erro ao conectar com o servidor.'
+const usuarioEncontrado = usuarios.find(u =>
+    u.usuario === nome &&
+    (u.email === usuario || u.numero === usuario) &&
+    u.senha === senha
+)
+
+const agenteEncontrado = agentes.find(a =>
+    a.name === nome &&
+    (a.email === usuario || a.phone_number === usuario) &&
+    a.senha === senha
+)
+
+if (usuarioEncontrado || agenteEncontrado) {
+    window.location.href = 'denuncias.html'
+} else {
+    document.getElementById('erro-geral').textContent = 'Dados incorretos.'
     document.getElementById('erro-geral').style.display = 'block'
 }
-    })
+
+    } catch (erro) {
+        document.getElementById('erro-geral').textContent = 'Erro ao conectar com o servidor.'
+        document.getElementById('erro-geral').style.display = 'block'
+    }
+})
 
